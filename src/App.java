0bcbbbfe6e82d4310b588050b6095e3b1834bd74
MatheusHLT <matheus.honorato.l.teixeira@gmail.com>
2026-03-08 18:16:36 -0300

    Exercicio05

---
+++ src/App.java
@@ -0,0 +1,24 @@
+// Nome: Matheus Honorato Leite Teixeira
+// Matricula: 1261929133
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        double celsius;
+
+        System.out.println("Digite a temperatura em Celsius:");
+        celsius = scanner.nextDouble();
+
+        double fahrenheit = (celsius * 9.0/5) + 32;
+        double kelvin = celsius + 273.15;
+
+        System.out.printf("Fahrenheit: %s °F", fahrenheit);
+        System.out.println();
+        System.out.printf("Kelvin: %s K", kelvin);
+
+        scanner.close();
+    }
+}
